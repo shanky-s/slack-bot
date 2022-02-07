8aab725fc3253bdd6254dae0e01eb8df51b87189
shanky-s <shashank.spinns@gmail.com>
2022-02-07 16:01:52 +0530

    error in logs in case of bot activation

--- routes/messages.js
+++ routes/messages.js
@@ -14,8 +14,8 @@ router.get("/", async function (req, res, next) {
 router.post("/", async function (req, res, next) {
   console.log(req.body);
 
-  res.statusCode = 200;
   if (req.body && req.body.command === "/bot") {
+    res.statusCode = 200;
     res.send({
       text: "Welcome. How are you doing?",
       response_type: "in_channel",
@@ -48,66 +48,67 @@ router.post("/", async function (req, res, next) {
         },
       ],
     });
-  }
+  } else {
+    res.statusCode = 200;
+    const { payload } = req.body;
+    if (!payload) {
+      res.statusCode = 400;
+      res.send();
+    }
+    console.log(payload);
 
-  const { payload } = req.body;
-  if (!payload) {
-    res.statusCode = 400;
-    res.send();
-  }
-  console.log(payload);
+    const slackResPayload = JSON.parse(payload);
+    const savedResponse = await userResponses.create(slackResPayload);
+    console.log("saved data");
+    console.log(savedResponse);
 
-  const slackResPayload = JSON.parse(payload);
-  const savedResponse = await userResponses.create(slackResPayload);
-  console.log("saved data");
-  console.log(savedResponse);
-
-  const { callback_id } = slackResPayload;
-  console.log(callback_id);
-  if (callback_id === "mood_selection") {
-    res.send({
-      text: "What are your favorite hobbies?",
-      response_type: "in_channel",
-      attachments: [
-        {
-          fallback: "If you could read this message, you'd be choosing something fun to do right now.",
-          color: "#4E5b31",
-          attachment_type: "default",
-          callback_id: "hobby_selection",
-          actions: [
-            {
-              name: "hobby_list",
-              type: "select",
-              options: [
-                {
-                  text: "Football",
-                  value: "football",
-                },
-                {
-                  text: "Music",
-                  value: "music",
-                },
-                {
-                  text: "Sleep",
-                  value: "sleep",
-                },
-                {
-                  text: "Movies",
-                  value: "movies",
-                },
-                {
-                  text: "Basketball",
-                  value: "basketball",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    });
-  }
-  if (callback_id === "hobby_selection") {
-    res.send("Thank You.");
+    const { callback_id } = slackResPayload;
+    console.log(callback_id);
+    if (callback_id === "mood_selection") {
+      res.send({
+        text: "What are your favorite hobbies?",
+        response_type: "in_channel",
+        attachments: [
+          {
+            fallback: "If you could read this message, you'd be choosing something fun to do right now.",
+            color: "#4E5b31",
+            attachment_type: "default",
+            callback_id: "hobby_selection",
+            actions: [
+              {
+                name: "hobby_list",
+                type: "select",
+                options: [
+                  {
+                    text: "Football",
+                    value: "football",
+                  },
+                  {
+                    text: "Music",
+                    value: "music",
+                  },
+                  {
+                    text: "Sleep",
+                    value: "sleep",
+                  },
+                  {
+                    text: "Movies",
+                    value: "movies",
+                  },
+                  {
+                    text: "Basketball",
+                    value: "basketball",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+    }
+    if (callback_id === "hobby_selection") {
+      res.send("Thank You.");
+    }
   }
 });
 
